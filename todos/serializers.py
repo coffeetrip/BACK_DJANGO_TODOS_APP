695e32d3e8cf64b24e54f0d1264af124364fef66
양예은 <yye7926@naver.com>
2020-02-05 21:01:06 +0900

    [+] todo user serializers.py

---
+++ todos/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Todo, User
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'user', 'title', 'completed']
+
+
+class UserSeriralizer(serializers.ModelSerializer):
+    todo_set = TodoSerializer(many=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'todo_set']
