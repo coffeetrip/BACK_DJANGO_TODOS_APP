c722dc85ba63a71aba4636577421e8d48c4e3048
양예은 <yye7926@naver.com>
2020-02-05 21:08:13 +0900

    [+] GET PUT DELETE

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
-from .serializer import *
+from .serializers import *
 from accounts.serializers import *
 from django.http import HttpResponse, JsonResponse, request
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -19,3 +19,23 @@ def todo_create(request):
         return JsonResponse(serializer.data)
         # return JsonResponse({'result':'true'})
     return HttpResponse(status=400)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JSONWebTokenAuthentication])
+def todo_detail(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'GET':
+        serializer = TodoSerializer(todo)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return HttpResponse(status=400)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return JsonResponse({'msg': '삭제되었습니다.'})
+        # return HttpResponse(status=204)
